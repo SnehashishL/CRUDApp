22ccb185fcb9c4dd4e7bf3b2040c2380ffb0bf97
Snehashish Lahiri <snehashish.lahiri@ibm.com>
2022-10-18 12:41:00 +0530

    added working restapi implementations --stable build

--- apis/apis.go
+++ apis/apis.go
@@ -1,12 +1,13 @@
 package apis
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // const (
@@ -16,8 +17,10 @@ import (
 // )
 
 // DB set up
-func SetupDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos")
+func SetupDB() *gorm.DB {
+	//db, err := sql.Open("mysql", "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos")
+	dsn := "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	// error handler
 	if err != nil {
 		//log.Print(err.Error())
@@ -27,15 +30,16 @@ func SetupDB() *sql.DB {
 	return db
 }
 
-type Task struct {
+type Todos struct {
+	//gorm.Model
 	TaskID   string `json:"taskid"`
 	TaskName string `json:"taskname"`
 }
 
 type JsonResponse struct {
-	Type    string `json:"type"`
-	Data    []Task `json:"data"`
-	Message string `json:"message"`
+	Type    string  `json:"type"`
+	Data    []Todos `json:"data"`
+	Message string  `json:"message"`
 }
 
 func printMessage(message string) {
@@ -68,9 +72,6 @@ func RouteInit() *mux.Router {
 	// Delete a specific task by the taskID
 	router.HandleFunc("/deleteTask/{taskid}", DeleteTask).Methods("DELETE")
 
-	// Delete all
-	router.HandleFunc("/deleteAll/", DeleteAll).Methods("DELETE")
-
 	// Update a task
 	router.HandleFunc("/updateTask/{taskid}", UpdateTask).Methods("PUT")
 
@@ -81,61 +82,36 @@ func RouteInit() *mux.Router {
 func AllTasks(w http.ResponseWriter, r *http.Request) {
 
 	db := SetupDB()
-	defer db.Close()
+	//defer db.Close()
 
 	printMessage("Getting ToDos...")
 
-	rows, err := db.Query("SELECT * FROM todos.todos")
-
-	CheckErr(err)
-
-	var tasks []Task
-
-	for rows.Next() {
-		var id int
-		var taskid string
-		var taskname string
-
-		err = rows.Scan(&id, &taskid, &taskname)
-
-		CheckErr(err)
-
-		tasks = append(tasks, Task{TaskID: taskid, TaskName: taskname})
-	}
-
-	var response = JsonResponse{Type: "success", Data: tasks}
-
-	json.NewEncoder(w).Encode(response)
+	//rows, err := db.Query("SELECT * FROM todos.todos")
+	var tasks []Todos
+	recs := db.Find(&tasks)
+	CheckErr(recs.Error)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 // Get Task by ID
 func GetTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-
 	taskID := params["taskid"]
 
 	db := SetupDB()
-	defer db.Close()
+	//defer db.Close()
 
-	printMessage("Getting ToDo with specified id ...")
+	printMessage("Getting ToDo by ID...")
 
-	//row := db.QueryRow("SELECT * FROM todos.todos WHERE taskID = {taskID}", taskID)
-	row := db.QueryRow("SELECT * FROM todos.todos where taskid = ?", taskID)
+	var task Todos
 
-	var task []Task
-	var id int
-	var taskid string
-	var taskname string
+	db.Find(&task, "task_id = ?", taskID)
+	fmt.Print(task.TaskID, task.TaskName)
+	//CheckErr(recs.Error)
 
-	err := row.Scan(&id, &taskid, &taskname)
-
-	CheckErr(err)
-
-	task = append(task, Task{TaskID: taskid, TaskName: taskname})
-
-	var response = JsonResponse{Type: "success", Data: task}
-
-	json.NewEncoder(w).Encode(response)
+	//var response = JsonResponse{Type: "success", Data: task}
+	json.NewEncoder(w).Encode(task)
 
 }
 
@@ -152,15 +128,14 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 
 		//db setup
 		db := SetupDB()
-		defer db.Close()
 
 		printMessage("Inserting task into DB")
 
 		fmt.Println("Inserting new task with ID: " + taskID + " and name: " + taskName)
 
 		//var lastInsertID int
-		_, err := db.Exec("INSERT INTO todos.todos(taskid, taskname) VALUES(?, ?)", taskID, taskName)
-		CheckErr(err)
+		task := Todos{TaskID: taskID, TaskName: taskName}
+		db.Create(&task)
 
 		response = JsonResponse{Type: "success", Message: "The task has been inserted successfully!"}
 	}
@@ -181,36 +156,18 @@ func DeleteTask(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// db setup
 		db := SetupDB()
-		defer db.Close()
+		//defer db.Close()
+
+		var task Todos
 
 		printMessage("Deleting Task from DB")
 
-		_, err := db.Exec("DELETE FROM todos.todos where taskID = ?", taskID)
-
-		CheckErr(err)
+		//_, err := db.Exec("DELETE FROM todos.todos where taskID = ?", taskID)
+		res := db.Where("task_id = ?", taskID).Delete(&task)
+		CheckErr(res.Error)
 
 		response = JsonResponse{Type: "success", Message: "The task has been deleted successfully!"}
 	}
-
-	json.NewEncoder(w).Encode(response)
-}
-
-// delete all tasks from db
-func DeleteAll(w http.ResponseWriter, r *http.Request) {
-
-	// db setup
-	db := SetupDB()
-	defer db.Close()
-
-	printMessage("Deleting all tasks...")
-
-	_, err := db.Exec("DELETE FROM todos.todos")
-
-	CheckErr(err)
-
-	printMessage("All tasks have been deleted successfully!")
-
-	var response = JsonResponse{Type: "success", Message: "All tasks have been deleted successfully!"}
 
 	json.NewEncoder(w).Encode(response)
 }
@@ -227,15 +184,20 @@ func UpdateTask(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// db setup
 		db := SetupDB()
-		defer db.Close()
+		//defer db.Close()
 
+		var task Todos
+		if err := db.Where("task_id = ?", taskID).First(&task).Error; err != nil {
+			CheckErr(err)
+			return
+		}
 		printMessage("Updating Task in DB")
 
 		fmt.Println("Updating new task with ID: " + taskID + " and name: " + taskName)
 
-		_, err := db.Exec("UPDATE todos.todos SET taskname = ? where taskID = ?", taskName, taskID)
+		res := db.Model(&task).Where("task_id = ?", taskID).Update("task_name", taskName)
 
-		CheckErr(err)
+		CheckErr(res.Error)
 
 		response = JsonResponse{Type: "success", Message: "The task has been updated successfully!"}
 	}
--- go.mod
+++ go.mod
@@ -5,4 +5,11 @@ go 1.19
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/mysql v1.4.3
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.24.0
 )
