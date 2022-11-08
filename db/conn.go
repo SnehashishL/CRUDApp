ec0ad8891a65ae31a5937c63970832f007e2bcce
Snehashish Lahiri <snehashish.lahiri@ibm.com>
2022-11-08 15:34:51 +0530

    migrated to postgresql from mysql

--- db/conn.go
+++ db/conn.go
@@ -1,7 +1,10 @@
 package db
 
 import (
-	"gorm.io/driver/mysql"
+	"fmt"
+
+	"github.com/SnehashishL/crudapp/models"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -12,26 +15,28 @@ const (
 )
 
 // // DB set up
-// func SetupDB() *sql.DB {
-// 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-// 	db, err := sql.Open("postgres", dbinfo)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	return db
-// }
-
-// DB set up
 func SetupDB() *gorm.DB {
-	//db, err := sql.Open("mysql", "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos")
-	dsn := "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// error handler
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	gdb, err := gorm.Open(postgres.Open(dbinfo), &gorm.Config{})
+
 	if err != nil {
 		panic(err)
 	}
-	//defer db.Close()
-	return db
+
+	gdb.AutoMigrate(&models.Todos{})
+
+	return gdb
 }
+
+// DB set up for mysql server
+// func SetupDB() *gorm.DB {
+// 	//db, err := sql.Open("mysql", "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos")
+// 	dsn := "root:sneh@mysql123@tcp(127.0.0.1:3306)/todos?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	// error handler
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	//defer db.Close()
+// 	return db
+// }
