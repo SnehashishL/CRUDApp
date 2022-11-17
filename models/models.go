927744efdae45b962c398528265b7c6bc39ce031
Snehashish-Lahiri <Snehashish.Lahiri@ibm.com>
2022-11-17 13:49:21 +0530

    models restructured --unstable

--- models/models.go
+++ models/models.go
@@ -1,9 +1,31 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Todos struct {
-	//gorm.Model
-	TaskID   string `json:"TaskID"`
-	TaskName string `json:"TaskName"`
+	gorm.Model
+	TaskID         string `json:"TaskID"`
+	Title          string `json:"TaskName"`
+	Description    string
+	Status         string
+	PlannedEndDate time.Time
+	TagID          int
+	UserID         string
+}
+
+type Tags struct {
+	TagID   int
+	TagName string
+}
+type Profiles struct {
+	ID        string
+	FirstName string
+	LastName  string
+	PIN       string
 }
 
 type JsonResponse struct {
