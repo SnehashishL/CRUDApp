dff04640b011977fa20da2fc2183733158095f6b
Snehashish-Lahiri <Snehashish.Lahiri@ibm.com>
2022-11-21 17:47:42 +0530

    removed TaskID from Todos table

--- models/models.go
+++ models/models.go
@@ -1,21 +1,19 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Todos struct {
 	gorm.Model
-	TaskID         string    `json:"taskId"`
-	Title          string    `json:"title"`
-	Priority       string    `json:"priority"`
-	Description    string    `json:"description"`
-	Status         string    `json:"status"`
-	PlannedEndDate time.Time `json:"plannedEndDate"`
-	TagID          int       `json:"tagId"`
-	UserID         string    `jason:"userId"`
+	//TaskID         string    `json:"taskId"`
+	Title          string `json:"title"`
+	Priority       string `json:"priority"`
+	Description    string `json:"description"`
+	Status         string `json:"status"`
+	PlannedEndDate string `json:"plannedEndDate"`
+	TagID          string `json:"tagId"`
+	UserID         string `jason:"userId"`
 }
 
 type Tags struct {
